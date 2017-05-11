39c192e49eca2cc0e22e111b843cd72dc1343ee7
Raymond Alvarez & Raymond Callahan <pair+RCAVelez.yellowjell0@devbootcamp.com>
2017-05-11 16:24:50 -0500

    Added CSS and some jquery logic

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,14 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+$(document).ready(function(){
+
+  ////Click a button and set the desired block to hit or miss. need some logic to teel if the class is a ship or not.
+  // if a ship is hit change the class to hit
+  // might need some ajax later once routes are ready
+  /// need to have the block id that will be shot at
+$('.fire-button').on('click',function(){
+  $block = $(this)
+})
+
+})
