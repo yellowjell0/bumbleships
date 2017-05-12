e8dcd2cd2810f63a55f5cdd4632785feb391392c
Raymond Alvarez <chve38@gmail.com>
2017-05-12 11:53:54 -0500

    Fixed some spelling erros

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,4 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
+
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
@@ -33,7 +22,7 @@ $(document).ready(function(){
 
   })
 
-  $('form').on('submit',function(e){
+  $('form.fire-button').on('submit',function(e){
     e.preventDefault();
     var block = $(this).siblings('.bottom').find('.selected');
 
